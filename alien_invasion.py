5cb93d9270fe58ce7b1eca1fd269d40eac14bcfd
David Phipps <david_phipps@volusion.com>
2017-03-01 15:26:45 -0600

    more aliens added

--- alien_invasion.py
+++ alien_invasion.py
@@ -26,7 +26,7 @@ def run_game():
     aliens = Group()
 
     # Create a flieet of aliens
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # Sart the main loop for the game.
     while True:
